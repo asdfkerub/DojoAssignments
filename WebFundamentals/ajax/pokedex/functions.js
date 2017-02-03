5cf7e7d526c677f9b14432f9ac9ee8ca6095b8c7
kerub <q.kerubkim@gmail.com>
2017-02-02 22:08:03 -0800

    Fixed / Add hover

--- WebFundamentals/ajax/pokedex/functions.js
+++ WebFundamentals/ajax/pokedex/functions.js
@@ -1,3 +1,15 @@
+// //On Hover color by Type
+//
+// $(document).on('hover','#main img',function(){
+//   var type = '';
+//   var url = "http://pokeapi.co/api/v1/pokemon/" + $(this).attr('id')+"/";
+//     $.get(url , function(res){
+//       console.log(res);
+//       type = res.types[0].name;
+//       console.log(type);
+//     },'json');
+// });
+
 $(document).ready(function(){
   for(var i = 1; i <= 151; i++){
     $('#wrapper #main').append('<img id='+ i + ' src="http://pokeapi.co/media/img/' + i + '.png">');
@@ -8,9 +20,9 @@ $(document).on('click','img',function(){
   $('#deck div').remove();
   var url = "http://pokeapi.co/api/v1/pokemon/" + $(this).attr('id')+"/";
   $.get(url , function(res){
+    //console.log(res);
     var type = '';
     var img = 'http://pokeapi.co/media/img/' + id + '.png';
-    console.log(id);
     for(var i = 0;i < res.types.length;i++){
       type+= '<li>' + res.types[i].name + '</li>';
     }
@@ -22,9 +34,8 @@ $(document).on('click','img',function(){
     $('#deck').append('<div class="pokemon"><h2>' + name +'</h2><img src="' + img + '"><p class="tit">Type(s)</p><ul>'+ type + '</ul><p class="tit">Height</p><p>' + height +'</p><p class="tit">Weight</p><p>' + weight +'</p></div>');
   },"json");
 
-$()
-
 });
+
 
 
 });
