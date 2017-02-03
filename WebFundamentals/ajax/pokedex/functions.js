ed1dd34525efc5678f6e6870b4f5d43950c4f2fc
kerub <q.kerubkim@gmail.com>
2017-02-02 21:42:56 -0800

    PokeMon Commit

---
+++ WebFundamentals/ajax/pokedex/functions.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  for(var i = 1; i <= 151; i++){
+    $('#wrapper #main').append('<img id='+ i + ' src="http://pokeapi.co/media/img/' + i + '.png">');
+  };
+
+$(document).on('click','img',function(){
+  var id = $(this).attr('id');
+  $('#deck div').remove();
+  var url = "http://pokeapi.co/api/v1/pokemon/" + $(this).attr('id')+"/";
+  $.get(url , function(res){
+    var type = '';
+    var img = 'http://pokeapi.co/media/img/' + id + '.png';
+    console.log(id);
+    for(var i = 0;i < res.types.length;i++){
+      type+= '<li>' + res.types[i].name + '</li>';
+    }
+    var name = res.name;
+    var pic = 'http://pokeapi.co' + res.sprites[1].resource_uri;
+    var height = res.height;
+    var weight = res.weight;
+
+    $('#deck').append('<div class="pokemon"><h2>' + name +'</h2><img src="' + img + '"><p class="tit">Type(s)</p><ul>'+ type + '</ul><p class="tit">Height</p><p>' + height +'</p><p class="tit">Weight</p><p>' + weight +'</p></div>');
+  },"json");
+
+$()
+
+});
+
+
+});
