d05b4a2a97456b1c0734384eba397a869d904305
kerub <q.kerubkim@gmail.com>
2017-02-04 08:57:03 -0800

    Changed folder name to testing for python testing purposes

---
+++ Python/testing/hello_world.py
@@ -0,0 +1,15 @@
+name = "Kerub"
+space = " "
+lname = "Q"
+
+print "Name is {}{}{}".format(name,space,lname);
+
+for x in range(11):
+  print "Testing"
+
+pip = "Why is my pip not working?"
+
+print name + " says: "  + pip
+print name + space + lname + " says: {}".format(pip)
+
+
