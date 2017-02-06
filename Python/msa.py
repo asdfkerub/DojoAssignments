ac4735f9bd97159483716de57639c9c4ac7ae5e2
kerub <q.kerubkim@gmail.com>
2017-02-06 12:13:12 -0800

    Multiple,sums,avg

---
+++ Python/msa.py
@@ -0,0 +1,8 @@
+for i in range(1,1000,2): #loops through 1- 1000 by 2 so it skips even numbers
+    print i #print the number
+for i in range(5,1000000,5): #loops through 5 - 1000000 by 5
+    print i #print the number
+
+a = [1, 2, 5, 10, 255, 3]
+print "Sum: " + str(sum(a)) #adds all index in a
+print "Avg: " + str(sum(a) / len(a)) #adds all index in athen divide by length of a
